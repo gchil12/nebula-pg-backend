0869e9871a2ad294e6259db7ea9aa0929eb2558e
Mariam Chilaia <mchil20@freeuni.edu.ge>
2022-08-11 09:26:34 +0400

    setChatbotkey

--- src/api/exam/controllers/exam.js
+++ src/api/exam/controllers/exam.js
@@ -8,25 +8,24 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::exam.exam", ({ strapi }) => ({
   setMessengerId: async (ctx, next) => {
-    // const { messengerId, chatbotKey } = ctx.request.body;
-    // const user = await strapi.db
-    //   .query("plugin::users-permissions.user")
-    //   .findOne({
-    //     where: { chatbotKey: chatbotKey },
-    //   });
-    // const entry = await strapi.db
-    //   .query("plugin::users-permissions.user")
-    //   .update({
-    //     where: { id: user.id },
-    //     data: {
-    //       messengerId: messengerId,
-    //     },
-    //   });
-    // return user.id;
+    const { messengerId, chatbotKey } = ctx.request.body;
+    const user = await strapi.db
+      .query("plugin::users-permissions.user")
+      .findOne({
+        where: { chatbotKey: chatbotKey },
+      });
+    const entry = await strapi.db
+      .query("plugin::users-permissions.user")
+      .update({
+        where: { id: user.id },
+        data: {
+          messengerId: messengerId,
+        },
+      });
+    return user.id;
   },
   setChatbotKey: async (ctx, next) => {
     const { userId, chatbotKey } = ctx.request.body.body;
-    console.log(ctx.request.body.body);
     const entry = await strapi.entityService.update(
       "plugin::users-permissions.user",
       userId,
